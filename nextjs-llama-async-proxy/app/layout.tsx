46b8232aca52d42deed4a24e2bb3fe01e7334261
Developer <user@example.com>
2025-12-11 18:05:54 +0700

    bye  custom theme provider

--- nextjs-llama-async-proxy/app/layout.tsx
+++ nextjs-llama-async-proxy/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "../src/components/ui/ThemeProvider";
 import { WebSocketProvider } from "../src/components/websocket/WebSocketManager";
 import Layout from "../src/components/layout/Layout";
 
